b2dbb22cfe669af5f91cdcaec8a467c71fc9ecf0
BahruzAlizada <behruzelzadee@gmail.com>
2023-06-20 21:34:32 +0400

    Init

---
+++ CarRent/CarRent/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using CarRent.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRent.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
